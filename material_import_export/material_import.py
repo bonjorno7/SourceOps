230e5e46b9a1bd790cc9ba70d401fc17cf0b548d
bonjorno7 <jorijndegraaf@gmail.com>
2019-08-15 16:36:04 +0200

    Start of cycles material builder
    gonna need red eye to help me with some stuff like how to find what kind of texture something is (basecolor, normal, gloss, envmapmask, etc)
    also I want to make it so that if it's lightmappedgeneric it creates a vertexlitgeneric version of it in materials/models/vmtpathhere

--- material_import_export/material_import.py
+++ material_import_export/material_import.py
@@ -20,6 +20,47 @@ class ImportMaterial(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
 
     files: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
 
+    def clear_node_tree(self, node_tree):
+        node_tree.nodes.clear()
+        node_tree.links.clear()
+        return node_tree
+
+    def output_node(self, nodes, x, y):
+        node = nodes.new("ShaderNodeOutputMaterial")
+        node.location = [x, y]
+        node.select = False
+        return node
+
+    def principled_node(self, nodes, x, y):
+        node = nodes.new("ShaderNodeBsdfPrincipled")
+        node.location = [x, y]
+        node.select = False
+        return node
+
+    def normal_map_node(self, nodes, x, y):
+        node = nodes.new("ShaderNodeNormalMap")
+        node.location = [x, y]
+        node.select = False
+        node.hide = True
+        return node
+
+    def image_node(self, nodes, image, x, y):
+        node = nodes.new("ShaderNodeTexImage")
+        node.location = [x, y]
+        node.select = False
+        node.image = image
+        node.hide = True
+        return node
+
+    def socket(self, node, identifier):
+        for socket in node.outputs:
+            if socket.identifier == identifier:
+                return socket
+
+        for socket in node.inputs:
+            if socket.identifier == identifier:
+                return socket
+
     def execute(self, context):
         game = common.get_game(context)
         if not game or not game.verify():
@@ -33,13 +74,37 @@ class ImportMaterial(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
             if not f.name.lower().endswith(".vmt"):
                 continue
 
-            material = vmt.VMT(folder / f.name, mod_folder)
-            material.parse()
-            # all used textures are in material.textures
-            # TODO: implement cycles material builder
-            pprint(material.textures)
-            for tex in material.textures.values():
-                import_vtf.import_texture(tex)
+            valve_mat = vmt.VMT(folder / f.name, mod_folder)
+            valve_mat.parse()
+
+            # all used textures are in valve_mat.textures
+            pprint(valve_mat.textures)
+
+            images = []
+            for tex in valve_mat.textures.values():
+                images.append(import_vtf.import_texture(tex))
+                #names = import_vtf.import_texture(tex)
+                #for n in names:
+                #    images.append(bpy.data.images[n])
+
+            mats_folder = Path(game.mod + os.sep + "materials" + os.sep)
+            blender_mat_name = Path(valve_mat.filepath).relative_to(mats_folder)
+            mat = bpy.data.materials.new(name=str(blender_mat_name)[:-4])
+
+            mat.use_nodes = True
+            node_tree = self.clear_node_tree(mat.node_tree)
+            nodes, links = node_tree.nodes, node_tree.links
+
+            node_output = self.output_node(nodes, 0, 0)
+            node_principled = self.principled_node(nodes, -400, 0)
+            node_normal_map = self.normal_map_node(nodes, -600, -520)
+
+            links.new(self.socket(node_output, "Surface"), self.socket(node_principled, "BSDF"))
+            links.new(self.socket(node_principled, "Normal"), self.socket(node_normal_map, "Normal"))
+
+            for image in images:
+                node_image = self.image_node(nodes, image, -900, -520)
+                links.new(self.socket(node_image, "Color"), self.socket(node_principled, "Base Color"))
 
         return {"FINISHED"}
 
