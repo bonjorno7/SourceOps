8f387f9f6a5a338e9707310d831d462a961c58a0
bonjorno7 <jorijndegraaf@gmail.com>
2020-03-19 15:50:47 +0100

    Change default material folder path
    I think this is better than leaving it blank

--- addon/props/material_folder_props.py
+++ addon/props/material_folder_props.py
@@ -11,5 +11,5 @@ class SOURCEOPS_MaterialFolderProps(bpy.types.PropertyGroup):
     path: bpy.props.StringProperty(
         name='Folder Path',
         description='$cdmaterials, the folder inside of which to look for materials, relative to your game\'s materials folder',
-        default='',
+        default='models/example',
     )
