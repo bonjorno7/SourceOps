e067e1c41b1b6f823c966406697fe3982be31fca
bonjorno7 <jorijndegraaf@gmail.com>
2020-03-19 16:08:40 +0100

    Bump version
    To go with commits 2adf5de through a5364d2

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, RED_EYE, SethTooQuick, Cabbage McGravel',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 80, 0),
-    'version': (0, 5, 0, 6),
+    'version': (0, 5, 0, 7),
     'location': '3D View > Sidebar',
     'category': 'Import-Export'
 }
