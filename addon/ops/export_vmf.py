0afa003cb0cc29f40319fbb3beed04daf9a5e3e3
bonjorno7 <jorijndegraaf@gmail.com>
2020-03-29 22:50:17 +0200

    More renaming and changes
    Turns out it needs to be per vertex, not per loop

--- addon/ops/export_vmf.py
+++ addon/ops/export_vmf.py
@@ -16,12 +16,18 @@ class SOURCEOPS_OT_ExportVMF(bpy.types.Operator):
         return bpy.data.is_saved and context.active_object and context.active_object.type == 'MESH'
 
     def invoke(self, context, event):
-        displacement_converter = displacement.DisplacementConverter(context.active_object)
+        displacement_converter = displacement.DispConverter(context.active_object)
         displacement_group = displacement_converter.displacement_group
 
-        for disp in displacement_group.displacements:
-            for row in disp:
-                print(', '.join(str(col.points[0].alpha) for col in row))
+        #for disp in displacement_group.displacements:
+        #    for row in disp:
+        #        print(', '.join(str(col.points[0].alpha) for col in row))
+
+        #for face in displacement_group.faces:
+        #    print(str(face.index) + ' : ' + ', '.join(f.index for f in face.neighbors if f))
+
+        for point in displacement_group.points:
+            print(str(point.index) + ' : ' + ', '.join(str(f.index) for f in point.faces))
 
         return {'FINISHED'}
 
--- addon/types/displacement.py
+++ addon/types/displacement.py
@@ -4,98 +4,90 @@ import mathutils
 from .. pyvmf import pyvmf
 
 
-class Vertex:
-    def __init__(self):
+class DispLoop:
+    def __init__(self, index):
+        self.index = index
+        self.xyz = [0, 0, 0]
+        self.uv = [0, 0]
+        self.alpha = 1.0
+        self.position = [0, 0, 0]
+        self.direction = [0, 0, 0]
+        self.distance = [0, 0, 0]
+
+
+class DispVertex:
+    def __init__(self, index):
+        self.index = index
         self.connected = []
         self.boundary = False
         self.corner = False
+        self.polygons = []
 
 
-class Point:
-    def __init__(self):
-        self.xyz = None
-        self.uv = None
-        self.alpha = None
-
-        self.boundary = False
-        self.corner = False
-        self.faces = []
-
-        self.position = None
-        self.direction = None
-        self.distance = None
-
-
-class Face:
-    def __init__(self):
-        self.points = []
+class DispPolygon:
+    def __init__(self, index):
+        self.index = index
+        self.vertices = []
         self.processed = False
         self.neighbors = [None] * 4
 
 
-class DisplacementGroup:
+class DispGroup:
     def __init__(self, mesh):
 
-        # Setup mesh, vertices, points, faces, and displacements
+        # Setup mesh, vertices, loops, polygons, and displacements
         self.mesh = mesh
-        self.vertices = [Vertex() for v in mesh.vertices]
-        self.points = [Point() for l in mesh.loops]
-        self.faces = [Face() for p in mesh.polygons]
+        self.loops = [DispLoop(i) for i in range(len(mesh.loops))]
+        self.vertices = [DispVertex(i) for i in range(len(mesh.vertices))]
+        self.polygons = [DispPolygon(i) for i in range(len(mesh.polygons))]
         self.displacements = []
 
         # Populate displacements
         self.get_xyz_and_uv_and_alpha()
         self.get_connected_and_marked()
         self.get_boundary_and_corner()
-        self.get_faces_and_points()
-        self.process_faces()
-        self.sort_faces()
+        self.get_polygons_and_vertices()
+        self.process_polygons()
+        self.sort_polygons()
         self.get_position_and_direction_and_distance()
 
 
-    def get_xyz_and_uv_and_alpha(self):
+        def get_xyz_and_uv_and_alpha(self):
 
-        # Iterate through mesh loops
-        for loop in self.mesh.loops:
+            # Iterate through mesh loops
+            for mesh_loop in self.mesh.loops:
 
-            # Get displacement point
-            point = self.points[loop.index]
+                # Get displacement loop
+                loop = self.loops[mesh_loop.index]
 
-            # Get XYZ
-            try:
-                point.xyz = self.mesh.vertices[loop.vertex_index].co[0:3]
-            except:
-                point.xyz = [0, 0, 0]
+                # Get XYZ
+                loop.xyz = mesh.vertices[mesh_loop.vertex_index].co[0:3]
 
-            # Get UV
-            try:
-                point.uv = self.mesh.uv_layers.active.data[loop.index].uv[0:2]
-            except:
-                point.uv = [0, 0]
+                # Get UV
+                if mesh.uv_layers:
+                    loop.uv = mesh.uv_layers.active.data[mesh_loop.index].uv[0:2]
 
-            # Get alpha
-            try:
-                point.alpha = self.mesh.vertex_colors.active.data[loop.index].color[0]
-            except:
-                point.alpha = 1.0
+                # Get alpha
+                if mesh.vertex_colors:
+                    loop.alpha = mesh.vertex_colors.active.data[mesh_loop.index].color[0]
 
 
     def get_connected_and_marked(self):
 
         # Iterate through mesh edges
-        for edge in self.mesh.edges:
+        for mesh_edge in self.mesh.edges:
 
             # Get temporary vertices in edge
-            vertex_a = self.vertices[edge.vertices[0]]
-            vertex_b = self.vertices[edge.vertices[1]]
+            vertex_a = self.vertices[mesh_edge.vertices[0]]
+            vertex_b = self.vertices[mesh_edge.vertices[1]]
 
             # Add vertices to each other's connected lists
             vertex_a.connected.append(vertex_b)
             vertex_b.connected.append(vertex_a)
 
             # If the edge is freestyle marked, its vertices are boundary
-            vertex_a.boundary |= edge.use_freestyle_mark
-            vertex_b.boundary |= edge.use_freestyle_mark
+            vertex_a.boundary |= mesh_edge.use_freestyle_mark
+            vertex_b.boundary |= mesh_edge.use_freestyle_mark
 
 
     def get_boundary_and_corner(self):
@@ -123,95 +115,90 @@ class DisplacementGroup:
             # If all of, or more than 2, connected vertices are boundary, this vert is a corner
             vertex.corner |= count > 2 or count == len(vertex.connected)
 
-        # Iterate through mesh loops
-        for loop in self.mesh.loops:
 
-            # Get displacement point and temporary vertex
-            point = self.points[loop.index]
-            vertex = self.vertices[loop.vertex_index]
-
-            # Get boundary and corner
-            point.boundary = vertex.boundary
-            point.corner = vertex.corner
-
-
-    def get_faces_and_points(self):
+    def get_polygons_and_vertices(self):
 
         # Iterate through mesh polygons
         for polygon in self.mesh.polygons:
 
+            # Get the displacement polygon
+            polygon = self.polygons[polygon.index]
+
             # Iterate through polygon loop indices
             for index in polygon.loop_indices:
 
-                # Store the points in the face
-                self.faces[polygon.index].points.append(self.points[index])
+                # Get the displacement vertex
+                vertex = self.vertices[self.mesh.loops[index].vertex_index]
 
-                # Store the face in the points
-                self.points[index].faces.append(self.faces[polygon.index])
+                # Store the vertices in the polygon
+                polygon.vertices.append(vertex)
 
-    # Rotate this face counter clockwise by the given amount of steps
-    def rotate_face(self, face, steps):
+                # Store the polygon in the vertices
+                vertex.polygons.append(polygon)
+
+    # Rotate this polygon counter clockwise by the given amount of steps
+    def rotate_polygon(self, polygon, steps):
 
         # Rotate the list to put this index at the start
-        face.points = face.points[steps:] + face.points[:steps]
+        polygon.vertices = polygon.vertices[steps:] + polygon.vertices[:steps]
 
-    # Find the faces on the left, top, right, and bottom of this face
-    def find_neighbors(self, face):
+    # Find the polygons on the left, top, right, and bottom of this polygon
+    def find_neighbors(self, polygon):
 
-        # If this face has been processed already
-        if face.processed:
+        # If this polygon has been processed already
+        if polygon.processed:
 
             # End the recursion
             return
 
-        # Make sure this face isn't processed again
-        face.processed = True
+        # Make sure this polygon isn't processed again
+        polygon.processed = True
 
-        # Iterate through the sides of this face
+        # Iterate through the sides of this polygon
         for side in range(4):
 
-            # Get the list of faces that the current point is in
-            faces = face.points[side].faces
+            # Get the list of polygons that the current vertex is in
+            polygons = polygon.vertices[side].polygons
 
-            # Get both points that are in the current edge
-            points = [face.points[side], face.points[(side + 1) % 4]]
+            # Get both vertices that are in the current edge
+            vertices = [polygon.vertices[side], polygon.vertices[(side + 1) % 4]]
 
-            # Get the unprocessed face of the current point that has both edge points
-            face.neighbors[side] = next((f for f in faces if not f.processed and all(v in f.points for v in points)), None)
+            # Get the unprocessed polygon of the current vertex that has both edge vertices
+            polygon.neighbors[side] = next((f for f in polygons if not f.processed and all(v in f.vertices for v in vertices)), None)
 
-            # If a face is found on the current side, and it hasn't already been processed
-            if face.neighbors[side] and not face.neighbors[side].processed:
+            # If a polygon is found on the current side, and it hasn't already been processed
+            if polygon.neighbors[side] and not polygon.neighbors[side].processed:
 
-                # Find the current point in this neighboring face
-                index = face.neighbors[side].points.index(face.points[side])
+                # Find the current vertex in this neighboring polygon
+                index = polygon.neighbors[side].vertices.index(polygon.vertices[side])
 
-                # Rotate this neighboring face so that the current point is in the correct corner
-                self.rotate_face(face, (index + 3) % 4)
+                # Rotate this neighboring polygon so that the current vertex is in the correct corner
+                self.rotate_polygon(polygon, (index + 3) % 4)
 
-                # Find the neighbors of this neighboring face
-                self.find_neighbors(face.neighbors[side])
+                # Find the neighbors of this neighboring polygon
+                self.find_neighbors(polygon.neighbors[side])
 
 
-    def process_faces(self):
+    def process_polygons(self):
 
-        # Keep going until all faces are processed
+        # Keep going until all polygons are processed
         while True:
 
-            # Start at any unsorted face
-            face = next((f for f in self.faces if not f.processed), None)
+            # Start at any unsorted polygon
+            polygon = next((f for f in self.polygons if not f.processed), None)
 
             # If none are found, we're done
-            if not face:
+            if not polygon:
                 break
 
-            # Process the face and its neighbords recursively
-            self.find_neighbors(face)
+            # Process the polygon and its neighbords recursively
+            self.find_neighbors(polygon)
 
 
-    def sort_faces(self):
+    def sort_polygons(self):
 
-        # Find bottom left corner faces
-        corners = [f for f in self.faces if f.points[0].corner]
+        # Find bottom left corner polygons
+        corners = [f for f in self.polygons if f.vertices[0].corner]
 
         # Iterate through those corners
         for corner in corners:
@@ -229,36 +216,36 @@ class DisplacementGroup:
                 grid.append([])
 
                 # Start at the edge
-                face = edge
+                polygon = edge
 
                 # Iterate through columns
                 for column in range(16):
 
-                    # Add this face to the grid
-                    grid[row].append(face)
+                    # Add this polygon to the grid
+                    grid[row].append(polygon)
 
                     # Stop at the end of the row
-                    if face.points[2].boundary and face.points[3].boundary:
+                    if polygon.vertices[2].boundary and polygon.vertices[3].boundary:
                         break
 
                     # Move to the right
-                    face = face.neighbors[2]
+                    polygon = polygon.neighbors[2]
 
                     # Stop at the end of the row??
-                    if not face:
-                        print('no face on the right')
+                    if not polygon:
+                        print('no polygon on the right')
                         break
 
                 # Stop at the end of the column
-                if edge.points[1].boundary and edge.points[2].boundary:
+                if edge.vertices[1].boundary and edge.vertices[2].boundary:
                     break
 
                 # Move upwards
                 edge = edge.neighbors[1]
 
                 # Stop at the end of the column??
-                if not face:
-                    print('no face above')
+                if not polygon:
+                    print('no polygon above')
                     break
 
             # Add the displacement to the list
@@ -268,11 +255,11 @@ class DisplacementGroup:
     def get_position_and_direction_and_distance(self):
 
         pass # Direction and distance
-        # TODO: Interpolate the position of the point on the brush face from the corners
-        # TODO: Calculate the direction and distance from that point to the new coordinates
+        # TODO: Interpolate the position of the vertex on the brush polygon from the corners
+        # TODO: Calculate the direction and distance from that vertex to the new coordinates
 
 
-class DisplacementConverter:
+class DispConverter:
     def __init__(self, object):
 
         # Switch to object mode
@@ -297,7 +284,7 @@ class DisplacementConverter:
         mesh.transform(object.matrix_world)
 
         # Sort the mesh into displacements
-        self.displacement_group = DisplacementGroup(mesh)
+        self.displacement_group = DispGroup(mesh)
 
         # Clear the evaluated mesh
         evaluated.to_mesh_clear()
