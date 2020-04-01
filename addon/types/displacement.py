99cd01d405b1c7b6aaf1d28d4c88ee6217bfed15
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-01 13:58:59 +0200

    Faster algorithm
    No need to get all neighbors when we only use two.
    Also it was inefficient to first sort faces and then loops, instead of combining that business into one function.

--- addon/types/displacement.py
+++ addon/types/displacement.py
@@ -28,93 +28,81 @@ class DispFace:
     def __init__(self, face):
         self.index = face.index
         self.loops = [loop.index for loop in face.loops]
-        self.neighbors = [self.find_neighbor(edge) for edge in face.edges]
-        self.boundaries = [self.find_boundary(edge) for edge in face.edges]
-        self.is_corner = self.boundaries[0] and self.boundaries[3]
+        self.face_above = self.find_face(face.edges[1])
+        self.face_right = self.find_face(face.edges[2])
+        self.edge_above = self.find_edge(face.edges[1])
+        self.edge_right = self.find_edge(face.edges[2])
+        self.is_corner = self.find_edge(face.edges[0]) and self.find_edge(face.edges[3])
 
-    def find_neighbor(self, edge):
+    def find_face(self, edge):
         return next((face.index for face in edge.link_faces if face.index != self.index), -1)
 
-    def find_boundary(self, edge):
+    def find_edge(self, edge):
         return edge.is_boundary or not edge.smooth
 
 
-class Disp:
+class DispInfo:
     def __init__(self, corner_face, disp_faces, disp_loops):
-        self.setup_face_grid(corner_face, disp_faces)
-        self.setup_loop_grid(disp_loops)
 
-    def setup_face_grid(self, corner_face, all_faces):
+        # Setup the grid
+        self.grid = []
 
-        # Setup face grid
-        self.face_grid = []
-
-        # Start in the corner
+        # Start at the corner
         edge_face = corner_face
 
         # Iterate through rows
         for row in range(16):
 
-            # Add an empty row
-            self.face_grid.append([])
+            # Add a new row
+            self.grid.append([])
+
+            # If we're on the last row
+            if edge_face.edge_above:
+
+                # Add another row
+                self.grid.append([])
 
             # Start at the edge
-            column_face = edge_face
+            current_face = edge_face
 
             # Iterate through columns
             for column in range(16):
 
-                # Add this polygon to the grid
-                self.face_grid[row].append(column_face)
+                # Add the bottom left loop of this face to this position
+                self.grid[row].append(disp_loops[current_face.loops[0]])
 
-                # Stop at the end of the row
-                if column_face.boundaries[2]:
+                # If we're on the last row
+                if current_face.edge_above:
+
+                    # Add the top left loop of this face to the position above
+                    self.grid[row + 1].append(disp_loops[current_face.loops[1]])
+
+                    # If we're also on the last column
+                    if current_face.edge_right:
+
+                        # Add the top right loop of this face to the position on the above right
+                        self.grid[row + 1].append(disp_loops[current_face.loops[2]])
+
+                # If we're on the last column
+                if current_face.edge_right:
+
+                    # Add the bottom right loop of this face to the position on the right
+                    self.grid[row].append(disp_loops[current_face.loops[3]])
+
+                    # And exit this row
                     break
 
-                # Move to the right
-                column_face = all_faces[column_face.neighbors[2]]
+                # Otherwise move to the right
+                current_face = disp_faces[current_face.face_right]
 
-            # Stop at the end of the column
-            if edge_face.boundaries[1]:
+            # If we're at the end of the column
+            if edge_face.edge_above:
+
+                # Exit the grid
                 break
 
-            # Move upwards
-            edge_face = all_faces[edge_face.neighbors[1]]
-
-    def setup_loop_grid(self, all_loops):
-
-        # Setup loop grid
-        self.loop_grid = []
-
-        # Determine the size of the face grid
-        size = len(self.face_grid)
-
-        # Iterate through face rows
-        for row in range(size):
-
-            # Add a new loop row
-            self.loop_grid.append([])
-
-            # Iterate through face columns
-            for column in range(size):
-
-                # Add the bottom left loop of each face
-                self.loop_grid[row].append(all_loops[self.face_grid[row][column].loops[0]])
-
-            # Add the bottom right loop of the last face
-            self.loop_grid[row].append(all_loops[self.face_grid[row][size - 1].loops[3]])
-
-        # Add an extra loop row
-        self.loop_grid.append([])
-
-        # Iterate through columns in the last face row
-        for column in range(size):
-
-            # Add the top left loop of each face
-            self.loop_grid[size].append(all_loops[self.face_grid[size - 1][column].loops[1]])
-
-        # Add the top right loop of the top right face
-        self.loop_grid[size].append(all_loops[self.face_grid[size - 1][size - 1].loops[2]])
+            # Otherwise move upwards
+            edge_face = disp_faces[edge_face.face_above]
 
 
 class DispGroup:
@@ -124,13 +112,10 @@ class DispGroup:
         bm = bmesh.new()
         bm.from_mesh(mesh)
 
-        # Setup loops and faces
+        # Setup loops, faces, and displacements
         self.loops = [DispLoop(mesh, loop) for loop in mesh.loops]
         self.faces = [DispFace(face) for face in bm.faces]
-
-        # Setup displacements
-        corners = [face for face in self.faces if face.is_corner]
-        self.displacements = [Disp(face, self.faces, self.loops) for face in corners]
+        self.displacements = [DispInfo(face, self.faces, self.loops) for face in self.faces if face.is_corner]
 
         # Populate displacements
         self.get_position_and_direction_and_distance()
