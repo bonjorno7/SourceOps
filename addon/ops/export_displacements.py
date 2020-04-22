ba16821749043f398c334435665aab6b8a8731b7
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-22 21:15:06 +0200

    Update ExportDisplacements operator to work with the new code
    Maps folder is stored in the game now, and you need to make a DispSettings to export displacements

--- addon/ops/export_displacements.py
+++ addon/ops/export_displacements.py
@@ -14,23 +14,36 @@ class SOURCEOPS_OT_ExportDisplacements(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         sourceops = common.get_globals(context)
+        game = common.get_game(sourceops)
         displacement_props = common.get_displacement_props(sourceops)
-        return sourceops and displacement_props
+        return sourceops and game and displacement_props
 
     def invoke(self, context, event):
         sourceops = common.get_globals(context)
+        game = common.get_game(sourceops)
         displacement_props = common.get_displacement_props(sourceops)
 
-        if not displacement_props.map_path:
-            self.report({'INFO'}, 'Please enter a file path')
+        if not game.maps:
+            self.report({'INFO'}, 'Please enter a maps folder')
+            return {'CANCELLED'}
+
+        if not displacement_props.name:
+            self.report({'INFO'}, 'Please enter a map name')
             return {'CANCELLED'}
 
         if not displacement_props.collection:
             self.report({'INFO'}, 'Please choose a collection')
             return {'CANCELLED'}
 
+        path = str(Path(game.maps).joinpath(displacement_props.name))
         objects = [o for o in displacement_props.collection.all_objects if o.type == 'MESH']
-        displacement.DispExporter(displacement_props, objects)
+
+        brush_scale = displacement_props.brush_scale
+        geometry_scale = displacement_props.geometry_scale
+        lightmap_scale = displacement_props.lightmap_scale
+
+        settings = displacement.DispSettings(path, objects, brush_scale, geometry_scale, lightmap_scale)
+        displacement.DispExporter(settings)
 
         self.report({'INFO'}, 'Exported VMF')
         return {'FINISHED'}
