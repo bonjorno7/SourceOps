bd47c1bfd7bb785acf556782b097f731125533be
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-23 12:23:11 +0200

    Add UIList for displacements

--- addon/ui/lists.py
+++ addon/ui/lists.py
@@ -41,3 +41,10 @@ class SOURCEOPS_UL_EventList(bpy.types.UIList):
 
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
         layout.prop(item, 'display', text='', emboss=False, translate=False)
+
+
+class SOURCEOPS_UL_DisplacementList(bpy.types.UIList):
+    bl_idname = 'SOURCEOPS_UL_DisplacementList'
+
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        layout.prop(item, 'display', text='', emboss=False, translate=False)
