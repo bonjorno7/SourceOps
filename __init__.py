c01c98148db6f15612c49493736545eda04aa77a
bonjorno7 <jorijndegraaf@gmail.com>
2020-04-25 17:03:14 +0200

    Bump version
    Added simulation rigging, commits f8c03fd through 8cd3267

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, Gorange, RED_EYE, SethTooQuick, Cabbage McGravel, Almaas',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 80, 0),
-    'version': (0, 5, 1, 7),
+    'version': (0, 5, 1, 8),
     'location': '3D View > Sidebar',
     'category': 'Import-Export'
 }
