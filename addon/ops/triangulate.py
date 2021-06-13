826ce373a7d03e8f7cd453ab2c50c3ba1ab088e8
bonjorno7 <jorijndegraaf@gmail.com>
2021-06-13 17:14:59 +0200

    Add operators for adding WN and TRI modifiers
    Weighted Normal is something is use super often.
    You should always use Triangulate, so you can have the same shading in Blender, Substance, Toolbag, Source, etc.
    I set my preferred defaults here, but you can change values from the redo panel so that's fine I think.
    I recommend adding Weighted Normal first, then Triangulate with Keep Normals.

---
+++ addon/ops/triangulate.py
@@ -0,0 +1,62 @@
+import bpy
+
+
+class SOURCEOPS_OT_triangulate(bpy.types.Operator):
+    bl_idname = 'sourceops.triangulate'
+    bl_label = 'Add Triangulate Modifier'
+    bl_description = 'Add a triangulate modifier to the selected objects and remove existing triangulate modifiers'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    quad_method: bpy.props.EnumProperty(
+        name='Quad Method',
+        description='Method for splitting the quads into triangles',
+        items=[
+            ('BEAUTY', 'Beauty', 'Split the quads in nice triangles, slower method'),
+            ('FIXED', 'Fixed', 'Split the quads on the first and third vertices'),
+            ('FIXED_ALTERNATE', 'Fixed Alternate', 'Split the quads on the 2nd and 4th vertice'),
+            ('SHORTEST_DIAGONAL', 'Shortest Diagonal', 'Split the quads based on the distance between the vertices'),
+        ],
+        default='BEAUTY',
+    )
+
+    ngon_method: bpy.props.EnumProperty(
+        name='N-gon Method',
+        description='Method for splitting the n-gons into triangles',
+        items=[
+            ('BEAUTY', 'Beauty', 'Arrange the triangles evenly (slow)'),
+            ('CLIP', 'Clip', 'Split the polygons with an ear clipping algorithm'),
+        ],
+        default='BEAUTY',
+    )
+
+    min_vertices: bpy.props.IntProperty(
+        name='Minimum Vertices',
+        description='Triangulate only polygons with vertex count greater than or equal to this number',
+        min=4,
+        default=4,
+    )
+
+    keep_custom_normals: bpy.props.BoolProperty(
+        name='Keep Normals',
+        description='Try to preserve custom normals.\nWarning: Depending on chosen triangulation method, shading may not be fully preserved, "Fixed" method usually gives the best result here',
+        default=True,
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type in {'MESH', 'CURVE', 'SURFACE', 'FONT'}:
+                for mod in obj.modifiers[:]:
+                    if mod.type == 'TRIANGULATE':
+                        obj.modifiers.remove(mod)
+
+                mod = obj.modifiers.new('Triangulate', 'TRIANGULATE')
+                mod.quad_method = self.quad_method
+                mod.ngon_method = self.ngon_method
+                mod.min_vertices = self.min_vertices
+                mod.keep_custom_normals = self.keep_custom_normals
+
+        return {'FINISHED'}
---
+++ addon/ops/weighted_normal.py
@@ -0,0 +1,71 @@
+import bpy
+
+
+class SOURCEOPS_OT_weighted_normal(bpy.types.Operator):
+    bl_idname = 'sourceops.weighted_normal'
+    bl_label = 'Add Weighted Normal Modifier'
+    bl_description = 'Add a weighted normal modifier to the selected objects and remove existing weighted normal modifiers'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    mode: bpy.props.EnumProperty(
+        name='Weighting Mode',
+        description='Weighted vertex normal mode to use',
+        items=[
+            ('FACE_AREA', 'Face Area', 'Generate face area weighted normals'),
+            ('CORNER_ANGLE', 'Corner Angle', 'Generate corner angle weighted normals'),
+            ('FACE_AREA_WITH_ANGLE', 'Face Area And Angle', 'Generated normals weighted by both face area and angle'),
+        ],
+        default='FACE_AREA',
+    )
+
+    weight: bpy.props.IntProperty(
+        name='Weight',
+        description='Corrective factor applied to faces’ weights, 50 is neutral, lower values increase weight of weak faces, higher values increase weight of strong faces',
+        min=1,
+        max=100,
+        default=50,
+    )
+
+    thresh: bpy.props.FloatProperty(
+        name='Threshold',
+        description='Threshold value for different weights to be considered equal',
+        min=0,
+        max=10,
+        default=0.01,
+    )
+
+    keep_sharp: bpy.props.BoolProperty(
+        name='Keep Sharp',
+        description='Keep sharp edges as computed for default split normals, instead of setting a single weighted normal for each vertex',
+        default=True,
+    )
+
+    use_face_influence: bpy.props.BoolProperty(
+        name='Face Influence',
+        description='Use influence of face for weighting',
+        default=False,
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
+    def execute(self, context):
+        bpy.ops.object.shade_smooth()
+
+        for obj in context.selected_objects:
+            if obj.type == 'MESH':
+                obj.data.use_auto_smooth = True
+
+                for mod in obj.modifiers[:]:
+                    if mod.type == 'WEIGHTED_NORMAL':
+                        obj.modifiers.remove(mod)
+
+                mod = obj.modifiers.new('WeightedNormal', 'WEIGHTED_NORMAL')
+                mod.mode = self.mode
+                mod.weight = self.weight
+                mod.thresh = self.thresh
+                mod.keep_sharp = self.keep_sharp
+                mod.use_face_influence = self.use_face_influence
+
+        return {'FINISHED'}
