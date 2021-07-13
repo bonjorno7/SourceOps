0b381709c5120dbd5714a4da0caaa5dae455f72b
bonjorno7 <jorijndegraaf@gmail.com>
2021-07-13 20:50:01 +0200

    Add prepend_armature and ignore_transforms to FBX
    I was worried I wouldn't be able to make these work, but it was not that hard.
    When you rename a bone in Blender, all vertex groups that depend on it are automatically renamed too.
    I tried setting matrix_world to Identity, but that had problems, so now I'm doing it with matrix_local and matrix_parent_inverse; haven't run into issues with this approach yet.
    To make export_scene.fbx behave properly, I had to get rid of leaf bones and animation (which is fine, cause they weren't used anyway).

--- addon/types/model_export/fbx.py
+++ addon/types/model_export/fbx.py
@@ -1,5 +1,6 @@
 import bpy
 from bpy.types import Object, TriangulateModifier
+from mathutils import Matrix
 from pathlib import Path
 from typing import List
 
@@ -8,7 +9,7 @@ MESH_TYPES = {'CURVE', 'FONT', 'MESH', 'SURFACE'}
 OBJECT_TYPES = MESH_TYPES | {'ARMATURE'}
 
 
-def export_fbx(path: Path, objects: List[Object]):
+def export_fbx(path: Path, objects: List[Object], prepend_armature: bool, ignore_transforms: bool):
     '''Export objects to FBX with settings for Source.'''
     frame_current = bpy.context.scene.frame_current
     bpy.context.scene.frame_current = bpy.context.scene.frame_start
@@ -20,6 +21,9 @@ def export_fbx(path: Path, objects: List[Object]):
     bpy.context.view_layer.active_layer_collection = bpy.context.view_layer.layer_collection.children[-1]
 
     obj_hide_viewport = {}
+    obj_matrix_local = {}
+    obj_matrix_parent_inverse = {}
+    bone_name = {}
     obj_triangulate_mod = {}
 
     for obj in objects:
@@ -27,6 +31,18 @@ def export_fbx(path: Path, objects: List[Object]):
             collection.objects.link(obj)
             obj_hide_viewport[obj] = obj.hide_viewport
             obj.hide_viewport = False
+
+            if ignore_transforms:
+                obj_matrix_local[obj] = obj.matrix_local.copy()
+                obj.matrix_local = Matrix.Identity(4)
+                obj_matrix_parent_inverse[obj] = obj.matrix_parent_inverse.copy()
+                obj.matrix_parent_inverse = Matrix.Identity(4)
+
+        if obj.type == 'ARMATURE':
+            if prepend_armature:
+                for bone in obj.data.bones:
+                    bone_name[bone] = bone.name
+                    bone.name = f'{obj.name}.{bone.name}'
 
         if obj.type in MESH_TYPES:
             mod: TriangulateModifier = obj.modifiers.new('Triangulate', 'TRIANGULATE')
@@ -37,13 +53,31 @@ def export_fbx(path: Path, objects: List[Object]):
             mod.keep_custom_normals = True
 
     try:
-        bpy.ops.export_scene.fbx(filepath=str(path), use_active_collection=True, global_scale=0.01)
+        bpy.ops.export_scene.fbx(
+            filepath=str(path),
+            use_active_collection=True,
+            global_scale=0.01,
+            add_leaf_bones=False,
+            bake_anim=False,
+        )
+
     except:
         raise
 
     finally:
         for obj, triangulate_mod in obj_triangulate_mod.items():
             obj.modifiers.remove(triangulate_mod)
+
+        if prepend_armature:
+            for bone, name in bone_name.items():
+                bone.name = name
+
+        if ignore_transforms:
+            for obj, matrix_parent_inverse in obj_matrix_parent_inverse.items():
+                obj.matrix_parent_inverse = matrix_parent_inverse
+
+            for obj, matrix_local in obj_matrix_local.items():
+                obj.matrix_local = matrix_local
 
         for obj, hide_viewport in obj_hide_viewport.items():
             obj.hide_viewport = hide_viewport
