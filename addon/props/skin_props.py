3c5ab06739c367fbd31373a59af522642e5eb780
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-02 14:37:35 +0200

    Clarify what the first skin must be
    This is something a lot of people don't understand; which is fair.

--- addon/props/skin_props.py
+++ addon/props/skin_props.py
@@ -4,6 +4,6 @@ import bpy
 class SOURCEOPS_SkinProps(bpy.types.PropertyGroup):
     name: bpy.props.StringProperty(
         name='Materials',
-        description='Space separated list of VMT names, supports quotes',
+        description='Space separated list of VMT names, supports quotes.\nThe first line must be the materials currently on your model',
         default='example',
     )
