391c3e4c614b0f2d112beb6a95beb1f39836be64
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-02 15:34:20 +0200

    Allow choosing steps for export auto
    Now you can choose which of the steps should be executed, and whether to do it for only the selected model or all models in the scene.
    If doing just the selected model, you can also open HLMV at the end.

--- addon/ops/export_auto.py
+++ addon/ops/export_auto.py
@@ -7,8 +7,26 @@ class SOURCEOPS_OT_ExportAuto(bpy.types.Operator):
     bl_idname = 'sourceops.export_auto'
     bl_options = {'REGISTER'}
     bl_label = 'Export Auto'
-    bl_description = '''Generate QC, export meshes, and compile QC
-Shift click to do this for all models in the scene'''
+    bl_description = 'Generate QC, export meshes, compile QC, view model'
+
+    all_models: bpy.props.BoolProperty(name='All Models', default=False)
+    generate_qc: bpy.props.BoolProperty(name='Generate QC', default=True)
+    export_meshes: bpy.props.BoolProperty(name='Export Meshes', default=True)
+    compile_qc: bpy.props.BoolProperty(name='Compile QC', default=True)
+    view_model: bpy.props.BoolProperty(name='View Model', default=False)
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column()
+        col.prop(self, 'all_models')
+
+        col.prop(self, 'generate_qc')
+        col.prop(self, 'export_meshes')
+        col.prop(self, 'compile_qc')
+
+        row = col.row()
+        row.enabled = not self.all_models
+        row.prop(self, 'view_model')
 
     @classmethod
     def poll(cls, context):
@@ -21,13 +39,19 @@ Shift click to do this for all models in the scene'''
     def invoke(self, context, event):
         prefs = utils.common.get_prefs(context)
         game = utils.common.get_game(prefs)
-        sourceops = utils.common.get_globals(context)
 
         if not utils.game.verify(game):
             self.report({'ERROR'}, 'Game is invalid')
             return {'CANCELLED'}
 
-        if event.shift:
+        return context.window_manager.invoke_props_dialog(self)
+
+    def execute(self, context):
+        prefs = utils.common.get_prefs(context)
+        game = utils.common.get_game(prefs)
+        sourceops = utils.common.get_globals(context)
+
+        if self.all_models:
             for model in sourceops.model_items:
                 if not self.export(game, model):
                     return {'CANCELLED'}
@@ -46,16 +70,20 @@ Shift click to do this for all models in the scene'''
     def export(self, game, model):
         source_model = Model(game, model)
 
-        if not source_model.generate_qc():
+        if self.generate_qc and not source_model.generate_qc():
             self.report({'ERROR'}, f'Failed to generate QC for {model.name}')
             return False
 
-        if not source_model.export_meshes():
+        if self.export_meshes and not source_model.export_meshes():
             self.report({'ERROR'}, f'Failed to export meshes for {model.name}')
             return False
 
-        if not source_model.compile_qc():
+        if self.compile_qc and not source_model.compile_qc():
             self.report({'ERROR'}, f'Failed to compile QC for {model.name}')
+            return False
+
+        if (not self.all_models and self.view_model) and not source_model.view_model():
+            self.report({'ERROR'}, f'Failed to open HLMV for {model.name}')
             return False
 
         return True
