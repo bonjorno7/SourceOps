03612ba7434d795a65e4ddb45d9f6a3fbdc9e82e
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-02 14:35:12 +0200

    Rename material folder Name to Relative Path
    This should make it more obvious what this is.

--- addon/props/material_folder_props.py
+++ addon/props/material_folder_props.py
@@ -3,7 +3,7 @@ import bpy
 
 class SOURCEOPS_MaterialFolderProps(bpy.types.PropertyGroup):
     name: bpy.props.StringProperty(
-        name='Name',
+        name='Relative Path',
         description='$cdmaterials, the folder inside of which to look for materials, relative to your game\'s materials folder',
         default='models/example',
     )
