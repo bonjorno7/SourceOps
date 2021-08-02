9b3fa939bbdfb8e0a6a44898198d450a1f6cc66e
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-02 14:10:34 +0200

    Adjust tooltips for game paths
    Just a bit of explanation for each path.

--- addon/props/game_props.py
+++ addon/props/game_props.py
@@ -11,35 +11,35 @@ class SOURCEOPS_GameProps(bpy.types.PropertyGroup):
 
     game: bpy.props.StringProperty(
         name='Game',
-        description='Path to your game folder',
+        description='Path to your game folder, gameinfo.txt is here',
         subtype='DIR_PATH',
         update=utils.game.update_game,
     )
 
     bin: bpy.props.StringProperty(
         name='Bin',
-        description='Path to your bin folder',
+        description='Path to your bin folder, EXE files are here',
         subtype='DIR_PATH',
         update=utils.game.update_bin,
     )
 
     modelsrc: bpy.props.StringProperty(
         name='ModelSrc',
-        description='Path to your modelsrc folder',
+        description='Path to your modelsrc folder, QC files go here',
         subtype='DIR_PATH',
         update=utils.game.update_modelsrc,
     )
 
     models: bpy.props.StringProperty(
         name='Models',
-        description='Path to your models folder',
+        description='Path to your models folder, MDL files go here',
         subtype='DIR_PATH',
         update=utils.game.update_models,
     )
 
     mapsrc: bpy.props.StringProperty(
         name='Mapsrc',
-        description='Path to your mapsrc folder',
+        description='Path to your mapsrc folder, VMF files go here',
         subtype='DIR_PATH',
         update=utils.game.update_mapsrc,
     )
