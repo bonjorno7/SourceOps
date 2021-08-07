7818eb3f497c84594eb60df410e223585f22ba11
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-07 22:35:37 +0200

    Bump version to 0.6.1
    This version adds the pose bone buttons to the Misc panel, in case people didn't know about them being in the pose mode right click menu.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, Almaas, Cabbage McGravel, Gorange, Krystian, RED_EYE, SethTooQuick, Yonder',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 80, 0),
-    'version': (0, 6, 0),
+    'version': (0, 6, 1),
     'location': '3D View > Sidebar',
     'category': 'Import-Export',
 }
