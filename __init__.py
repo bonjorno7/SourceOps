66f060cb61d5911af64ae50ae2d93b748aabd9b1
bonjorno7 <jorijndegraaf@gmail.com>
2022-11-04 23:10:25 +0100

    Bump version to 0.6.9
    Nice.
    This version makes it so $origin is not used when static prop combine is enabled.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, Almaas, Cabbage McGravel, CryptAlchemy, Gorange, Krystian, RED_EYE, SethTooQuick, Yonder',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 83, 0),
-    'version': (0, 6, 8),
+    'version': (0, 6, 9),
     'location': '3D View > Sidebar',
     'category': 'Import-Export',
 }
