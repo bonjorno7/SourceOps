6ff3c3e3cd13e3b0834e8b964cb308735acd7efa
bonjorno7 <jorijndegraaf@gmail.com>
2025-08-22 14:55:06 +0200

    Bump version 0.7.6
    This update fixes displacement export for modern version of Blender.
    Thanks to @wouterpleizier for his contribution!

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, Almaas, Cabbage McGravel, CryptAlchemy, Gorange, Krystian, RED_EYE, SethTooQuick, Yonder',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 83, 0),
-    'version': (0, 7, 5),
+    'version': (0, 7, 6),
     'location': '3D View > Sidebar',
     'category': 'Import-Export',
 }
