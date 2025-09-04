4072fc31149069c6ffc641abe385516e29546160
bonjorno7 <jorijndegraaf@gmail.com>
2025-09-04 21:51:41 +0200

    Bump version to 0.7.7
    This update adds support for the $mass command.
    Thanks to @Glad-BR for his contribution!

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'author': 'bonjorno7, Almaas, Cabbage McGravel, CryptAlchemy, Gorange, Krystian, RED_EYE, SethTooQuick, Yonder, Blueberry_pie, Glad_BR',
     'description': 'A more convenient alternative to Blender Source Tools',
     'blender': (2, 83, 0),
-    'version': (0, 7, 6),
+    'version': (0, 7, 7),
     'location': '3D View > Sidebar',
     'category': 'Import-Export',
 }
